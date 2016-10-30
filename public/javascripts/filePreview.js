f5fe67070dc05ace7ab360ba54c2f063faaeea15
paulcarroll <pvcarroll@gmail.com>
2016-10-29 23:55:09 -0500

    es6 class component with constructor

--- public/javascripts/filePreview.js
+++ public/javascripts/filePreview.js
@@ -1,21 +1,32 @@
-var FilePreview = React.createClass({
-  handleFileChange: function(e) {
+class FilePreview extends React.Component{
+  constructor(props) {
+    super(props);
+    this.handleFileChange = this.handleFileChange.bind(this);
+    this.state = {
+      files: []
+    };
+  }
+
+  handleFileChange(e) {
     let reader = new FileReader();
     let file = e.target.files[0];
-    let thisReference = this;
     reader.onload = function() {
-      thisReference.setState({
+      let newFile = {
         file: file,
         filePreviewUrl: reader.result
+      };
+      this.setState({
+        files: newFile.filePreviewUrl
+        // files: this.state.files.concat(newFile)
       });
     };
     reader.readAsDataURL(file);
-  },
+  }
 
-  render: function() {
+  render() {
     let filePreview = "";
     if (this.state) {
-      filePreview = this.state.filePreviewUrl;
+      filePreview = <img src={this.state.files} />;
     }
     return (
         <div>
@@ -24,6 +35,6 @@ var FilePreview = React.createClass({
         </div>
     );
   }
-});
+}
 
 ReactDOM.render(<FilePreview />, document.getElementById("filePreviewContainer"));
