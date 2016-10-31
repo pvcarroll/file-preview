68d72dc745989f300f319cb7e3685cd02d1f784f
paulcarroll <pvcarroll@gmail.com>
2016-10-30 22:25:06 -0500

    FilePreview with filename

--- public/javascripts/filePreview.js
+++ public/javascripts/filePreview.js
@@ -1,4 +1,16 @@
-class FilePreview extends React.Component{
+class FileWidget extends React.Component {
+  render() {
+    return (
+        <div>
+          <img src="" className="fileIcon"/>
+          <span className="fileName">{this.props.fileName}</span>
+          <span className="fileType"></span>
+        </div>
+    );
+  }
+}
+
+class FilePreview extends React.Component {
   constructor(props) {
     super(props);
     this.handleFileChange = this.handleFileChange.bind(this);
@@ -10,11 +22,17 @@ class FilePreview extends React.Component{
   handleFileChange(e) {
     let reader = new FileReader();
     let file = e.target.files[0];
+    let fullFileName = e.target.value;
+    let extIndex = fullFileName.lastIndexOf(".");
+    let fileName = (extIndex !== -1) ? fullFileName.substring(0, extIndex) : fullFileName;
+    let fileExt = fullFileName.substring(extIndex + 1);
     let thisReference = this;
     reader.onload = function() {
       let newFile = {
         file: file,
-        filePreviewUrl: reader.result
+        filePreviewUrl: reader.result,
+        fileName: fileName,
+        fileExt: fileExt
       };
       thisReference.setState({
         files: thisReference.state.files.concat(newFile)
@@ -24,14 +42,19 @@ class FilePreview extends React.Component{
   }
 
   render() {
-    let filePreview = "";
-    if (this.state && this.state.files[0]) {
-      filePreview = <img src={this.state.files[0].filePreviewUrl} />;
-    }
+    let files = this.state.files;
     return (
         <div>
           <input type="file" onChange={this.handleFileChange} />
-          <div>{filePreview}</div>
+          {
+              files.map(function(file) {
+                return <FileWidget key={file.fileName}
+                                   file={file.file}
+                                   filePreviewUrl={file.filePreviewUrl}
+                                   fileName={file.fileName}
+                                   fileExt={file.fileExt} />;
+              })
+          }
         </div>
     );
   }
